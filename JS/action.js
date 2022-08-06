e26176d41f156ee837ad5c0bd0fa9c2ba0489e7c
Rony J. Letona <zronyj@gmail.com>
2022-08-06 01:04:50 -0600

    Part 2
    6. Diagramar la pagina
    7. Agregar estilos a diferentes partes
    8. Agregar texto dummy
    9. Crear metodos para mostrar/ocultar segmentos
    10. Ver la pagina por segunda vez con interactividad

--- JS/action.js
+++ JS/action.js
@@ -3,7 +3,17 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            aboutShow: false,
+            dnaShow: true,
+            aaShow: false,
+            spinnerShow: false,
+            resultsShow: false,
             message: 'Hello World!'
+        }
+    },
+    methods: {
+        show(something) {
+            this[something] = ! this[something];
         }
     }
 }).mount('#app')
