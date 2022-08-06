279fdd147618ed16feb693067c89358b610fd7f4
Rony J. Letona <zronyj@gmail.com>
2022-08-06 01:07:31 -0600

    Making it pretty
    Adding some Bootstrap elements to make it prettier.

--- JS/action.js
+++ JS/action.js
@@ -4,16 +4,26 @@ createApp({
     data() {
         return {
             aboutShow: false,
+            howtoShow: true,
             dnaShow: true,
             aaShow: false,
             spinnerShow: false,
             resultsShow: false,
-            message: 'Hello World!'
+            message: 'Hello Marlene!'
         }
     },
     methods: {
         show(something) {
-            this[something] = ! this[something];
+            this.aboutShow = false;
+            this.howtoShow = false;
+            this.dnaShow = false;
+            this.aaShow = false;
+            this.spinnerShow = false;
+            this.resultsShow = false;
+            if ((something == "dnaShow") || (something == "aaShow")) {
+                this.howtoShow = true;
+            }
+            this[something] = true;
         }
     }
 }).mount('#app')
