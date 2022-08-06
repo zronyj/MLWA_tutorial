8c0f91f8a4236ea5e1dca18d018292afeae86490
Rony J. Letona <zronyj@gmail.com>
2022-08-06 02:04:06 -0600

    Part 3
    11. Crear el backend y arrancarlo
    12. Agregar secuencias
    13. Crear rutas para cada secuencia
    14. Devolver las secuencias como objetos en cada ruta
    15. Probarlo en el navegador

---
+++ Backend/mlwa_server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+# DNA Sequence (predicted)
+dna_seq = "atgagctattatcatcatcatcatcatcatgattatgatattccgaccaccgaaaacctgtattttcagggcgcgatgggcattctgggcagcggccagaaacattttgaaaaacgccgcaacccggcggcgggcctgattcagagcgcgtggcgcttttatgcgaccaacctgagccgcaccgatctgcatagcacctggcagtattatgaacgcaccgtgaccgtgccgatgtatcgcggcctggaagatctgaccccgggcctgaaagtgagcattcgcgcggtgtgcgtgatgcgctttctggtgagcaaacgcaaatttaaagaaagcctgcgcctggat"
+dna_seq = dna_seq.upper()
+
+# Amino Acid Sequence
+# https://www.ncbi.nlm.nih.gov/protein/6FEH_A
+aa_seq = "msyyhhhhhhdydipttenlyfqgamgilgsgqkhfekrrnpaagliqsawrfyatnlsrtdlhstwqyyertvtvpmyrgledltpglkvsiravcvmrflvskrkfkeslrld"
+aa_seq = aa_seq.upper()
+
+app = Flask(__name__)
+
+# Route for the URL "/"
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World! How are you?</p>"
+
+# Route for the URL "/dnaseq" --> DNA
+@app.route("/dnaseq")
+def dna_sequence():
+    return dna_seq
+
+# Route for the URL "/aaseq" --> Amino Acid
+@app.route("/aaseq")
+def aa_sequence():
+    return aa_seq
+
+if __name__ == "__main__":
+    app.run(debug=True)
