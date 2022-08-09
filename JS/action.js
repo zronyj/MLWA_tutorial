e60a0d20fa7403383282bf7d31ac4f8b15609234
Rony J. Letona <zronyj@gmail.com>
2022-08-08 21:18:16 -0600

    Part 5
    21. Pre-cargar imagenes al cache
    22. Agregar slider y botones
    23. Hacer link entre las secuencias y las imagenes
    24. Mostrar imagenes dependiendo de la secuencia
    25. Ver la pagina con las imagenes interactivas segun secuencia

--- JS/action.js
+++ JS/action.js
@@ -6,6 +6,38 @@ createApp({
             endpoint: "http://127.0.0.1:5000/",
             aaSequence: ["G"],
             dnaSequence: ["A"],
+            aaMut: {
+                "G": ["Glycine", "glycine.png"],
+                "A": ["Alanine", "alanine.png"],
+                "L": ["Leucine", "leucine.png"],
+                "M": ["Methionine", "methionine.png"],
+                "F": ["Phenylalanine", "phenylalanine.png"],
+                "W": ["Tryptophan", "tryptophan.png"],
+                "K": ["Lysine", "lysine.png"],
+                "Q": ["Glutamine", "glutamine.png"],
+                "E": ["Glutamic Acid", "glutamic_acid.png"],
+                "S": ["Serine", "serine.png"],
+                "P": ["Proline", "proline.png"],
+                "V": ["Valine", "valine.png"],
+                "I": ["Isoleucine", "isoleucine.png"],
+                "C": ["Cysteine", "cysteine.png"],
+                "Y": ["Tyrosine", "tyrosine.png"],
+                "H": ["Histidine", "histidine.png"],
+                "R": ["Arginine", "arginine.png"],
+                "N": ["Asparagine", "asparagine.png"],
+                "D": ["Aspartic Acid", "aspartic_acid.png"],
+                "T": ["Threonine", "threonine.png"]
+            },
+            dnaMut: {
+                "A": ["Adenine", "adenine.png"],
+                "C": ["Thymine", "thymine.png"],
+                "G": ["Guanine", "guanine.png"],
+                "T": ["Cytosine", "cytosine.png"]
+            },
+            dna: 0,
+            aa: 0,
+            aaImgs: {},
+            dnaImgs: {},
             aboutShow: false,
             howtoShow: true,
             dnaShow: true,
@@ -57,5 +89,16 @@ createApp({
         };
         getAASequence();
         getDNASequence();
+    },
+    // This will be done right after the app has been mounted
+    mounted() {
+        Object.keys(this.aaMut).forEach(element => {
+            this.aaImgs[element] = new Image();
+            this.aaImgs[element].src = "IMG/" + this.aaMut[element][1];
+        });
+        Object.keys(this.dnaMut).forEach(element => {
+            this.dnaImgs[element] = new Image();
+            this.dnaImgs[element].src = "IMG/" + this.dnaMut[element][1];
+        });
     }
 }).mount('#app')
